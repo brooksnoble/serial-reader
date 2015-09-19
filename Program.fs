77e368a0c3bc29441c9def5b0bd7deff30c98d2d
brooksnoble <brooks@Brookss-MacBook-Pro.local>
2015-09-19 10:58:16 -0400

    Basic serial reading

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+open System.IO.Ports
+
+[<EntryPoint>]
+let main argv = 
+    let myPort = new SerialPort()
+
+    myPort.PortName <- "/dev/tty.usbmodemfd121"
+    myPort.BaudRate <- 9600
+    myPort.DataBits <- 8
+    myPort.Parity <- Parity.None
+    myPort.StopBits <- StopBits.One
+
+    myPort.Open()
+
+    while true do
+        let value = myPort.ReadTo("\r\n")
+
+        match System.Int32.TryParse(value) with
+        | (true, value) ->
+            printfn "%d" value
+        | _ -> 
+            printfn "Bad: %s" value
+
+    myPort.Close()
+
+    0
